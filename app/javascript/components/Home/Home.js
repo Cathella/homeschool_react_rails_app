725cec25235e85b37a91abdef02a3bf95468b81c
Cathella <ncathie214@gmail.com>
2021-08-02 15:57:51 +0300

    root view updates

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import styled from 'styled-components'
+import Logo from 'images/logo.svg'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`
+const Nav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const AppLogo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  span {
+    text-transform: uppercase;
+    color: black;
+    font-weight: bold;
+  }
+
+  img {
+    width: 45px;
+    margin-right: 10px;
+  }
+`
 
 const Home = () => {
   return(
-    <div>This the home#index view for our app</div>
+    <Container>
+      <Nav>
+        <Link to={`/`}>
+          <AppLogo>
+            <img src={Logo} />
+            <span>Homeschool</span>
+          </AppLogo>
+        </Link>
+        
+        <div>
+          <a href="">Teachers</a>
+          <a href="">Children</a>
+          <a href="">Lessons</a>
+          <a href="">Signup</a>
+        </div>
+      </Nav>
+    </Container>
   )
 }
 
